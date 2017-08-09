001bd40d8c59902da5813b6adbabf137094001ac
EwingTsai <191237878@qq.com>
2017-08-10 01:27:55 +0800

    自定义权限控制逻辑

--- src/main/java/ewing/demo/ThymeleafUser.java
+++ src/main/java/ewing/demo/ThymeleafUser.java
@@ -40,7 +40,7 @@ public class ThymeleafUser {
     }
 
     @GetMapping("deleteUser")
-    @PreAuthorize("hasPermission(#userId,'USER')")
+    @PreAuthorize("hasPermission(#userId,'ROLE_USER')")
     public ModelAndView deleteUser(Integer userId) {
         userService.deleteUser(userId);
         return index();
