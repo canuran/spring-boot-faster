8aaff8c1380658e4ab9dd9f58d33e299ee4b8e70
EwingTsai <191237878@qq.com>
2017-10-12 13:53:19 +0800

    优化Demo类

--- src/main/java/ewing/demo/SimpleController.java
+++ src/main/java/ewing/demo/SimpleController.java
@@ -1,8 +1,11 @@
 package ewing.demo;
 
 import ewing.application.RequestMessage;
+import ewing.common.JWTUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -14,6 +17,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class SimpleController {
 
+    /**
+     * 枚举测试。
+     */
+    public enum Gender {
+        BOY, GIRL
+    }
+
+    @ResponseBody
+    @GetMapping("getType")
+    public Gender exception(Gender gender) {
+        return gender;
+    }
+
+    /**
+     * 异常捕获测试。
+     */
     @ResponseBody
     @GetMapping("exception")
     public void exception() {
@@ -32,6 +51,19 @@ public class SimpleController {
     @GetMapping("language")
     public String language() {
         return RequestMessage.getMessage("language");
+    }
+
+    /**
+     * 获取JWT测试。
+     */
+    @PostMapping("getJWT")
+    public String getJWT(String name) {
+        return JWTUtils.generateToken("name", name);
+    }
+
+    @PostMapping("checkJWT")
+    public String checkJWT(@RequestHeader("Authorization") String token) {
+        return (String) JWTUtils.getFromToken(token, "name");
     }
 
 }
