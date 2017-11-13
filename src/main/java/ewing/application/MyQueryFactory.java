61105ecf3e34fceceda8eee7cfd9a414ea082744
EwingTsai <191237878@qq.com>
2017-11-13 10:49:05 +0800

    直接通过ID操作实体

--- src/main/java/ewing/application/MyQueryFactory.java
+++ src/main/java/ewing/application/MyQueryFactory.java
@@ -11,12 +11,13 @@ import com.querydsl.sql.RelationalPathBase;
 import com.querydsl.sql.SQLQueryFactory;
 
 import javax.inject.Provider;
-import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.util.List;
 
 /**
- * 查询工厂。
+ * 扩展查询工厂，支持通过非联合ID操作实体。
+ *
+ * @author Ewing
  */
 public class MyQueryFactory extends SQLQueryFactory {
 
@@ -24,34 +25,91 @@ public class MyQueryFactory extends SQLQueryFactory {
         super(configuration, connProvider);
     }
 
+    /**
+     * 根据ID查询实体。
+     */
     public <E> E selectById(RelationalPathBase<E> base, Object value) {
         return this.selectFrom(base)
-                .where(idEquals(base, value))
+                .where(pathEquals(getPrimaryPath(base), value))
                 .fetchOne();
     }
 
-    private <E> BooleanExpression idEquals(RelationalPathBase<E> base, Object value) {
-        PrimaryKey primaryKey = base.getPrimaryKey();
-        List<? extends Path<?>> paths = primaryKey.getLocalColumns();
-        if (paths.size() == 1) {
-            return pathEquals(paths.get(0), value);
-        } else if (paths.size() > 1) {
-            BooleanExpression expression = null;
-            for (Path<?> path : paths) {
-                String name = path.getMetadata().getName();
-                Method method = ReflectionUtils.getGetterOrNull(value.getClass(), name);
-                try {
-                    Object prop = method.invoke(value);
-                    BooleanExpression express = pathEquals(path, prop);
-                    expression = expression == null ? express : expression.and(express);
-                } catch (ReflectiveOperationException e) {
-                    throw new RuntimeException("Reflective operation exception.", e);
-                }
-            }
-            return expression;
-        } else {
-            throw new RuntimeException("Primary key path is empty.");
+    /**
+     * 根据实体中的ID属性删除实体。
+     */
+    public <E> long deleteEntity(RelationalPathBase<E> base, E entity) {
+        Object value = readPrimaryKey(entity, getPrimaryPath(base));
+        return this.delete(base)
+                .where(pathEquals(getPrimaryPath(base), value))
+                .execute();
+    }
+
+    /**
+     * 根据ID删除实体。
+     */
+    public long deleteById(RelationalPathBase base, Object value) {
+        return this.delete(base)
+                .where(pathEquals(getPrimaryPath(base), value))
+                .execute();
+    }
+
+    /**
+     * 根据对象中的ID属性更新实体。
+     */
+    public long updateEntity(RelationalPathBase base, Object entity) {
+        Object value = readPrimaryKey(entity, getPrimaryPath(base));
+        return this.update(base)
+                .populate(entity)
+                .where(pathEquals(getPrimaryPath(base), value))
+                .execute();
+    }
+
+    /**
+     * 插入一个实体。
+     */
+    public <E> long insert(RelationalPathBase<E> base, E entity) {
+        return this.insert(base)
+                .populate(entity)
+                .execute();
+    }
+
+    /**
+     * 插入一个实体并返回ID值。
+     */
+    public Object insertWithId(RelationalPathBase base, Object entity) {
+        return this.insert(base)
+                .populate(entity)
+                .executeWithKey(getPrimaryPath(base));
+    }
+
+    private Path<?> getPrimaryPath(RelationalPathBase base) {
+        if (base == null) {
+            throw new IllegalArgumentException("PathBase is null.");
         }
+        PrimaryKey primaryKey = base.getPrimaryKey();
+        if (primaryKey == null) {
+            throw new IllegalArgumentException("Primary key is null.");
+        }
+        List<?> paths = primaryKey.getLocalColumns();
+        if (paths == null) {
+            throw new IllegalArgumentException("Primary paths is null.");
+        }
+        if (paths.size() == 1) {
+            return (Path) paths.get(0);
+        } else {
+            throw new RuntimeException("Primary path is not unique.");
+        }
+    }
+
+    private <E> Object readPrimaryKey(E entity, Path path) {
+        Object value;
+        try {
+            value = ReflectionUtils.getGetterOrNull(entity.getClass(),
+                    path.getMetadata().getName()).invoke(entity);
+        } catch (Exception e) {
+            throw new RuntimeException("Read primary key failed.", e);
+        }
+        return value;
     }
 
     private BooleanExpression pathEquals(Path path, Object value) {
@@ -60,9 +118,8 @@ public class MyQueryFactory extends SQLQueryFactory {
         } else if (path instanceof StringPath && value instanceof String) {
             return ((StringPath) path).eq((String) value);
         } else {
-            throw new RuntimeException("Path or value is unsupported.");
+            throw new IllegalArgumentException("Path or value is unsupported.");
         }
     }
-
 
 }
