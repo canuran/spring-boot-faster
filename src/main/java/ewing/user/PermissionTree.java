e2969ce7726dbdefa84189a5b0697ee1b4369eeb
EwingTsai <191237878@qq.com>
2017-12-05 16:24:16 +0800

    支持树节点列表转换成树结构

--- src/main/java/ewing/user/PermissionTree.java
+++ src/main/java/ewing/user/PermissionTree.java
@@ -1,5 +1,6 @@
 package ewing.user;
 
+import ewing.application.common.TreeNode;
 import ewing.entity.Permission;
 
 import java.util.List;
@@ -9,14 +10,21 @@ import java.util.List;
  *
  * @author 权限树节点。
  */
-public class PermissionTree extends Permission {
+public class PermissionTree extends Permission implements TreeNode<PermissionTree, Long> {
 
     private List<PermissionTree> children;
 
+    @Override
+    public Long getId() {
+        return getPermissionId();
+    }
+
+    @Override
     public List<PermissionTree> getChildren() {
         return children;
     }
 
+    @Override
     public void setChildren(List<PermissionTree> children) {
         this.children = children;
     }
