5b6f0a6666f81ee40b81be617af4602ebc8b218e
EwingTsai <191237878@qq.com>
2017-12-29 16:02:05 +0800

    异步调用线程池配置

---
+++ src/main/java/ewing/application/config/ThreadExecutorConfig.java
@@ -0,0 +1,32 @@
+package ewing.application.config;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@EnableAsync
+@Configuration
+public class ThreadExecutorConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(2);
+        taskExecutor.setKeepAliveSeconds(3600);
+        taskExecutor.setMaxPoolSize(50);
+        taskExecutor.setQueueCapacity(500);
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new SimpleAsyncUncaughtExceptionHandler();
+    }
+
+}
