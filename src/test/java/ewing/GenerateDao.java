f2a84500074b3320c2ec60c7eb866be520863c15
caiyouyuan <caiyouyuan@bilibili.com>
2018-06-12 19:17:26 +0800

    支持用代码生成Dao层

--- src/test/java/ewing/GenerateDao.java
+++ src/test/java/ewing/GenerateDao.java
@@ -1,20 +1,76 @@
 package ewing;
 
+import com.querydsl.maven.AbstractMetaDataExportMojo;
+import com.querydsl.maven.MetadataExportMojo;
+import com.querydsl.sql.codegen.support.NumericMapping;
+import ewing.application.config.SBFBasisDao;
 import ewing.application.query.DaoGenerator;
+import org.apache.maven.project.MavenProject;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
 
 /**
- * 自动生成Dao接口与实现，先用Maven插件export后再执行Dao生成。
+ * 自动生成Dao接口与实现，比Maven配置更灵活。
  *
  * @author Ewing
  * @since 2018年05月21日
  */
 public class GenerateDao {
 
-    public static void main(String[] args) {
+    /***
+     * 生成Bean和QBean。
+     */
+    @Test
+    public void generateMeta() throws Exception {
+        // 配置参数并导出代码
+        MetadataExportMojo exporter = new MetadataExportMojo();
+        MavenProject project = new MavenProject();
+        project.getProperties().setProperty("project.build.sourceEncoding", "UTF-8");
+        exporter.setProject(project);
+        exporter.setJdbcUrl("jdbc:mysql://localhost:3306/faster?useUnicode=true&characterEncoding=UTF-8");
+        exporter.setJdbcDriver("com.mysql.jdbc.Driver");
+        exporter.setJdbcUser("faster");
+        exporter.setJdbcPassword("faster");
+        exporter.setExportBeans(true);
+        exporter.setNamePrefix("Q");
+        exporter.setTargetFolder("src/main/java");
+        exporter.setPackageName("ewing.dao1.query");
+        exporter.setBeanPackageName("ewing.dao1.entity");
+        exporter.setBeanInterfaces(new String[]{java.io.Serializable.class.getName()});
+        Field beanAddToString = AbstractMetaDataExportMojo.class.getDeclaredField("beanAddToString");
+        beanAddToString.setAccessible(true);
+        beanAddToString.set(exporter, true);
+        exporter.setNumericMappings(getNumericMappings());
+        exporter.setTableNamePattern("%");
+        exporter.execute();
+    }
+
+    /***
+     * 生成Dao层代码。
+     */
+    @Test
+    public void generateDao() throws Exception {
         new DaoGenerator()
-                .daoPackage("ewing.dao")
-                .queryBeanPackage("ewing.dao.query")
+                .daoSuperClass(SBFBasisDao.class)
+                .javaCodePath("src/main/java")
+                .daoPackage("ewing.dao1")
+                .queryBeanPackage("ewing.dao1.query")
                 .generate();
+    }
+
+    /**
+     * 5位以下的整数统一使用Integer。
+     */
+    private static NumericMapping[] getNumericMappings() {
+        NumericMapping[] numericMappings = new NumericMapping[5];
+        for (int i = 0; i < numericMappings.length; i++) {
+            numericMappings[i] = new NumericMapping();
+            numericMappings[i].setDecimal(0);
+            numericMappings[i].setTotal(i);
+            numericMappings[i].setJavaType(Integer.class.getName());
+        }
+        return numericMappings;
     }
 
 }
