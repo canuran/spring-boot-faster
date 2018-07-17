3156c07c3c773234e1e42534a665c2c946e2deb4
caiyouyuan <caiyouyuan@bilibili.com>
2018-07-17 19:19:33 +0800

    默认页面优化

--- src/main/java/ewing/application/config/WebErrorController.java
+++ src/main/java/ewing/application/config/WebErrorController.java
@@ -3,9 +3,6 @@ package ewing.application.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.autoconfigure.web.ErrorController;
-import org.springframework.boot.autoconfigure.web.ErrorProperties;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -29,24 +26,20 @@ import java.util.Map;
  */
 @Controller
 @ApiIgnore
-@EnableConfigurationProperties(ServerProperties.class)
-@RequestMapping({"${server.error.path:${error.path:error}}"})
+@RequestMapping({"${server.error.path:error}"})
 public class WebErrorController implements ErrorController {
 
     private ErrorAttributes errorAttributes;
-
-    private ServerProperties serverProperties;
 
     /**
      * 初始化WebErrorController
      */
     @Autowired
-    public WebErrorController(ErrorAttributes errorAttributes, ServerProperties serverProperties) {
-        if (errorAttributes == null || serverProperties == null) {
+    public WebErrorController(ErrorAttributes errorAttributes) {
+        if (errorAttributes == null) {
             throw new IllegalArgumentException("错误页面初始化失败：参数为空！");
         }
         this.errorAttributes = errorAttributes;
-        this.serverProperties = serverProperties;
     }
 
     /**
@@ -74,7 +67,7 @@ public class WebErrorController implements ErrorController {
     @ResponseBody
     @RequestMapping
     public ResponseEntity<Map<String, Object>> errorJson(HttpServletRequest request) {
-        Map<String, Object> model = getErrorAttributes(request, hasStackTrace(request));
+        Map<String, Object> model = getErrorAttributes(request);
         HttpStatus status = getHttpStatus(request);
         Map<String, Object> body = new HashMap<>(4);
         body.put("code", status.value());
@@ -85,20 +78,11 @@ public class WebErrorController implements ErrorController {
     }
 
     /**
-     * 是否包括堆栈跟踪属性
-     */
-    private boolean hasStackTrace(HttpServletRequest request) {
-        ErrorProperties.IncludeStacktrace include = this.serverProperties.getError().getIncludeStacktrace();
-        return include == ErrorProperties.IncludeStacktrace.ALWAYS ||
-                (include == ErrorProperties.IncludeStacktrace.ON_TRACE_PARAM && this.hasStackTrace(request));
-    }
-
-    /**
      * 获取错误的信息
      */
-    private Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean includeStackTrace) {
+    private Map<String, Object> getErrorAttributes(HttpServletRequest request) {
         RequestAttributes requestAttributes = new ServletRequestAttributes(request);
-        return this.errorAttributes.getErrorAttributes(requestAttributes, includeStackTrace);
+        return this.errorAttributes.getErrorAttributes(requestAttributes, false);
     }
 
     /**
@@ -106,11 +90,8 @@ public class WebErrorController implements ErrorController {
      */
     private HttpStatus getHttpStatus(HttpServletRequest request) {
         Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
-        if (statusCode == null) {
-            return HttpStatus.INTERNAL_SERVER_ERROR;
-        }
         try {
-            return HttpStatus.valueOf(statusCode);
+            return statusCode == null ? HttpStatus.INTERNAL_SERVER_ERROR : HttpStatus.valueOf(statusCode);
         } catch (Exception ex) {
             return HttpStatus.INTERNAL_SERVER_ERROR;
         }
