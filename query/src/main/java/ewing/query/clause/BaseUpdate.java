0d53c3a375f2f677294578f4fce76d0331e436f3
caiyouyuan <caiyouyuan@bilibili.com>
2018-10-31 18:13:44 +0800

    优化基本查询类

--- query/src/main/java/ewing/query/clause/BaseUpdate.java
+++ query/src/main/java/ewing/query/clause/BaseUpdate.java
@@ -1,6 +1,7 @@
 package ewing.query.clause;
 
 import com.querydsl.core.types.Path;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.sql.Configuration;
 import com.querydsl.sql.RelationalPath;
 import com.querydsl.sql.RelationalPathBase;
@@ -13,6 +14,8 @@ import javax.inject.Provider;
 import java.sql.Connection;
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * 增强的更新语句。
@@ -27,6 +30,48 @@ public class BaseUpdate extends AbstractSQLUpdateClause<BaseUpdate> {
 
     public BaseUpdate(Provider<Connection> connection, Configuration configuration, RelationalPath<?> entity) {
         super(connection, configuration, entity);
+    }
+
+    /**
+     * 如果测试值为真则添加条件。
+     */
+    public BaseUpdate whereIfTrue(boolean test, Supplier<Predicate> getPredicate) {
+        return test ? where(getPredicate.get()) : this;
+    }
+
+    /**
+     * 如果值存在则添加条件。
+     */
+    public <T> BaseUpdate whereIfNotNull(T value, Function<T, Predicate> getPredicate) {
+        return value == null ? this : where(getPredicate.apply(value));
+    }
+
+    /**
+     * 如果字符串有值则添加条件。
+     */
+    public <T extends CharSequence> BaseUpdate whereIfHasLength(T value, Function<T, Predicate> getPredicate) {
+        return value != null && value.length() > 0 ? where(getPredicate.apply(value)) : this;
+    }
+
+    /**
+     * 如果字符串不为空白字符则添加条件。
+     */
+    public <T extends CharSequence> BaseUpdate whereIfHasText(T value, Function<T, Predicate> getPredicate) {
+        if (value != null && value.length() > 0) {
+            for (int i = 0; i < value.length(); ++i) {
+                if (!Character.isWhitespace(value.charAt(i))) {
+                    return where(getPredicate.apply(value));
+                }
+            }
+        }
+        return this;
+    }
+
+    /**
+     * 如果集合不为空则添加条件。
+     */
+    public <T extends Collection<O>, O> BaseUpdate whereIfNotEmpty(T value, Function<T, Predicate> getPredicate) {
+        return value != null && value.size() > 0 ? where(getPredicate.apply(value)) : this;
     }
 
     /**
