6221369ec05a6fa6b8c23a6a42a344cbc4cfa0a4
caiyouyuan <caiyouyuan@bilibili.com>
2018-11-01 14:40:27 +0800

    优化基本查询类

--- faster/src/test/java/ewing/faster/GenerateQuery.java
+++ faster/src/test/java/ewing/faster/GenerateQuery.java
@@ -1,6 +1,6 @@
 package ewing.faster;
 
-import com.querydsl.maven.AbstractMetaDataExportMojo;
+import com.querydsl.core.util.ReflectionUtils;
 import com.querydsl.maven.MetadataExportMojo;
 import com.querydsl.sql.codegen.support.NumericMapping;
 import org.apache.maven.project.MavenProject;
@@ -9,7 +9,7 @@ import java.lang.reflect.Field;
 import java.util.stream.IntStream;
 
 /**
- * 自动生成Dao接口与实现，比Maven配置更灵活。
+ * 生成实体以及查询对象的工具类。
  *
  * @author Ewing
  * @since 2018年05月21日
@@ -17,7 +17,7 @@ import java.util.stream.IntStream;
 public class GenerateQuery {
 
     /***
-     * 生成Bean和QBean。
+     * 生成实体以及查询对象，比Maven插件灵活些。
      */
     public static void main(String[] args) throws Exception {
         // 配置参数并导出代码
@@ -35,12 +35,8 @@ public class GenerateQuery {
         exporter.setPackageName("ewing.faster.dao.query");
         exporter.setBeanPackageName("ewing.faster.dao.entity");
         exporter.setBeanInterfaces(new String[]{java.io.Serializable.class.getName()});
-        Field beanAddToString = AbstractMetaDataExportMojo.class.getDeclaredField("beanAddToString");
-        beanAddToString.setAccessible(true);
-        beanAddToString.set(exporter, true);
-        Field exportPrimaryKeys = AbstractMetaDataExportMojo.class.getDeclaredField("exportPrimaryKeys");
-        exportPrimaryKeys.setAccessible(true);
-        exportPrimaryKeys.set(exporter, true);
+        setFieldValue(exporter, "beanAddToString", true);
+        setFieldValue(exporter, "exportPrimaryKeys", true);
         exporter.setCustomTypes(new String[]{com.querydsl.sql.types.UtilDateType.class.getName()});
         exporter.setNumericMappings(getNumericMappings());
         exporter.setTableNamePattern("%");
@@ -58,6 +54,19 @@ public class GenerateQuery {
             mapping.setJavaType(Integer.class.getName());
             return mapping;
         }).toArray(NumericMapping[]::new);
+    }
+
+    /**
+     * 设置对象的属性值。
+     */
+    private static void setFieldValue(Object target, String fieldName, Object value) {
+        try {
+            Field field = ReflectionUtils.getFieldOrNull(target.getClass(), fieldName);
+            field.setAccessible(true);
+            field.set(target, value);
+        } catch (Exception e) {
+            throw new IllegalStateException("Set field value fail", e);
+        }
     }
 
 }
