2be3ebc2aea0ee03a3c038e3e8972ba37d51055e
caiyouyuan <caiyouyuan@bilibili.com>
2019-06-15 18:30:52 +0800

    添加Excel导出工具类

---
+++ common/src/main/java/ewing/common/excel/ExcelColumn.java
@@ -0,0 +1,19 @@
+package ewing.common.excel;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * ExcelSheet列信息。
+ *
+ * @author caiyouyuan
+ * @since 2019年06月15日
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExcelColumn {
+
+    String value() default "";
+
+    int columnWidth() default 0;
+
+}
