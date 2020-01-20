ae0275c91a0e66a2d2abd592c09ffb9476982402
EwingTsai <191237878@qq.com>
2020-01-21 03:50:55 +0800

    全局ID优化

--- common/src/main/java/ewing/common/snowflake/DatabaseAutoInstanceSupplier.java
+++ common/src/main/java/ewing/common/snowflake/DatabaseAutoInstanceSupplier.java
@@ -23,27 +23,51 @@ import java.util.function.IntSupplier;
 @SuppressWarnings({"SqlDialectInspection", "SqlNoDataSourceInspection"})
 public class DatabaseAutoInstanceSupplier implements IntSupplier {
     private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseAutoInstanceSupplier.class);
+    private static final String TABLE_NAME = "snowflake_id_instance";
+    private static final String DB_TIME = "unix_timestamp(current_timestamp(3)) * 1000";
+    private static final String APPLY_SQL = "update " + TABLE_NAME + " set version = version + 1," +
+            " owner_id = ?, expire = " + DB_TIME + " + ? where id = ? and version = ?";
+    private static final String QUERY_MINE = "select id, min(instance) as instance, version" +
+            " from " + TABLE_NAME + " where owner_id = ? and expire > " + DB_TIME + " + 1000";
+    private static final String QUERY_EXPIRE = "select id, min(instance) as instance, version" +
+            " from " + TABLE_NAME + " where expire < " + DB_TIME + " - 1000 limit 1";
 
-    // 实例编号有效期100秒
-    private static final long VALID_TIME = 100000L;
-    // 每30秒续约或者更新
-    private static final long REFRESH_TIME = 30000L;
+    // 实例编号有效期
+    private static final long VALID_TIME = 300000L;
+    // 续约或者更新间隔
+    private static final long REFRESH_TIME = 60000L;
 
     private final DataSource dataSource;
+    private final String applySql;
+    private final String queryMine;
+    private final String queryExpire;
     private final String owner;
     private int instance = -1;
     private long expire = 0L;
 
     public DatabaseAutoInstanceSupplier(DataSource dataSource) {
+        this(dataSource, TABLE_NAME);
+    }
+
+    private DatabaseAutoInstanceSupplier(DataSource dataSource, String tableName) {
         if (dataSource == null) {
             throw new IllegalArgumentException("DataSource wrong");
         }
+        if (tableName == null) {
+            throw new IllegalArgumentException("Table prefix wrong");
+        }
+
         // 32位的36进制数最大为166位
         byte[] bytes = new byte[21];
         new SecureRandom().nextBytes(bytes);
         bytes[0] = 0b00011111; // 高165位设为1，高于165位设为0
         this.owner = new BigInteger(bytes).toString(36);
+
         this.dataSource = dataSource;
+        this.applySql = APPLY_SQL.replace(TABLE_NAME, tableName);
+        this.queryMine = QUERY_MINE.replace(TABLE_NAME, tableName);
+        this.queryExpire = QUERY_EXPIRE.replace(TABLE_NAME, tableName);
+
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
@@ -60,55 +84,39 @@ public class DatabaseAutoInstanceSupplier implements IntSupplier {
         return instance;
     }
 
-    private static final String DB_TIME = "unix_timestamp(current_timestamp(3)) * 1000";
-
-    private static final String APPLY_SQL = "update snowflake_id_instance" +
-            " set version = version + 1, owner_id = ?," +
-            " expire = " + DB_TIME + " + ? where id = ? and version = ?";
-
-    private static final String QUERY_SQL = "select id, min(instance) as instance, version" +
-            " from snowflake_id_instance where owner_id = ? and expire > " + DB_TIME + " + 1000" +
-            " union select id, min(instance) as instance, version" +
-            " from snowflake_id_instance where expire < " + DB_TIME + " - 1000";
-
     private void refresh() {
         try {
             long update = 0;
             long time = System.currentTimeMillis();
             // 每秒尝试1次，共3次机会
             for (int i = 0; i < 3; i++) {
-                Integer queryId = null;
-                Integer queryInstance = null;
-                Long version = null;
+                QueryInstance queryInstance;
                 try (Connection connection = dataSource.getConnection()) {
-                    // 获取正在使用或者可用的实例编号
-                    try (PreparedStatement statement = connection.prepareStatement(QUERY_SQL)) {
+                    // 获取正在使用的实例编号
+                    try (PreparedStatement statement = connection.prepareStatement(queryMine)) {
                         statement.setString(1, owner);
-                        try (ResultSet resultSet = statement.executeQuery()) {
-                            while (resultSet.next()) {
-                                if (queryId != null || queryInstance != null || version != null) {
-                                    break;
-                                }
-                                queryId = getQueryResult(resultSet, "id", Integer.class);
-                                queryInstance = getQueryResult(resultSet, "instance", Integer.class);
-                                version = getQueryResult(resultSet, "version", Long.class);
-                            }
+                        queryInstance = statementQueryInstance(statement);
+                    }
+                    if (queryInstance == null) {
+                        // 获取可用的实例编号
+                        try (PreparedStatement statement = connection.prepareStatement(queryExpire)) {
+                            queryInstance = statementQueryInstance(statement);
                         }
                     }
                     // 使用乐观锁占用获取到的编号
-                    if (queryId != null && queryInstance != null && version != null) {
-                        if (queryInstance < 0 || queryInstance > SnowflakeIdWorker.MAX_INSTANCE) {
+                    if (queryInstance != null) {
+                        if (queryInstance.getId() < 0 || queryInstance.getId() > SnowflakeIdWorker.MAX_INSTANCE) {
                             throw new IllegalStateException("Query instance value wrong");
                         }
-                        try (PreparedStatement statement = connection.prepareStatement(APPLY_SQL)) {
+                        try (PreparedStatement statement = connection.prepareStatement(applySql)) {
                             int index = 1;
                             statement.setString(index++, owner);
                             statement.setLong(index++, VALID_TIME);
-                            statement.setInt(index++, queryId);
-                            statement.setLong(index, version);
+                            statement.setInt(index++, queryInstance.getId());
+                            statement.setLong(index, queryInstance.getVersion());
                             update = statement.executeUpdate();
                             if (update > 0) {
-                                this.instance = queryInstance;
+                                this.instance = queryInstance.getInstance();
                                 expire = time + VALID_TIME;
                                 break;
                             }
@@ -131,10 +139,51 @@ public class DatabaseAutoInstanceSupplier implements IntSupplier {
         }
     }
 
-    @SuppressWarnings("unchecked")
-    private <T> T getQueryResult(ResultSet resultSet, String name, Class<T> type) throws SQLException {
-        Object object = resultSet.getObject(name);
-        return type.isInstance(object) ? (T) object : null;
+    private QueryInstance statementQueryInstance(PreparedStatement statement) throws SQLException {
+        try (ResultSet resultSet = statement.executeQuery()) {
+            if (resultSet.next()) {
+
+                Object objectId = resultSet.getObject("id");
+                Integer queryId = objectId instanceof Integer ? (Integer) objectId : null;
+
+                Object objectInstance = resultSet.getObject("instance");
+                Integer queryInstance = objectInstance instanceof Integer ? (Integer) objectInstance : null;
+
+                Object objectVersion = resultSet.getObject("version");
+                Long queryVersion = objectVersion instanceof Long ? (Long) objectVersion : null;
+
+                if (queryId == null || queryInstance == null || queryVersion == null) {
+                    return null;
+                } else {
+                    return new QueryInstance(queryId, queryInstance, queryVersion);
+                }
+            }
+            return null;
+        }
+    }
+
+    private static class QueryInstance {
+        private Integer id;
+        private Integer instance;
+        private Long version;
+
+        QueryInstance(Integer id, Integer instance, Long version) {
+            this.id = id;
+            this.instance = instance;
+            this.version = version;
+        }
+
+        Integer getId() {
+            return id;
+        }
+
+        Integer getInstance() {
+            return instance;
+        }
+
+        Long getVersion() {
+            return version;
+        }
     }
 
 }
