9c0cb7796b106540b8539b20ebda6c00f3a80d66
caiyouyuan <191237878@qq.com>
2020-02-27 16:46:30 +0800

    添加TestTuple

---
+++ query/src/main/java/ewing/query/support/TestTuple.java
@@ -0,0 +1,37 @@
+package ewing.query.support;
+
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Expression;
+
+import java.util.LinkedHashMap;
+
+/**
+ * 单元测试专用Tuple。
+ *
+ * @author Ewing
+ * @since 2020年02月20日
+ */
+@SuppressWarnings("unchecked")
+public class TestTuple extends LinkedHashMap<Expression<?>, Object> implements Tuple {
+
+    public <T> TestTuple put(Expression<T> key, T value) {
+        super.put(key, value);
+        return this;
+    }
+
+    @Override
+    public <T> T get(int index, Class<T> type) {
+        return (T) values().stream().skip(index)
+                .findFirst().orElse(null);
+    }
+
+    @Override
+    public <T> T get(Expression<T> expr) {
+        return (T) super.get(expr);
+    }
+
+    @Override
+    public Object[] toArray() {
+        return values().toArray();
+    }
+}
